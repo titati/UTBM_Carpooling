ee310a61ba196b5f2599dc080e17833621a345f2
Boko <bartheleway@live.fr>
2013-05-26 01:59:37 +0200

    Fix driver car item

--- src/fr/utbm/carpooling/view/widgets/DriverCarAdapter.java
+++ src/fr/utbm/carpooling/view/widgets/DriverCarAdapter.java
@@ -3,13 +3,9 @@ package fr.utbm.carpooling.view.widgets;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-import fr.utbm.carpooling.R;
 import fr.utbm.carpooling.model.DriverCar;
 
 public class DriverCarAdapter extends ArrayAdapter<DriverCar> {
@@ -24,43 +20,18 @@ public class DriverCarAdapter extends ArrayAdapter<DriverCar> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View v = convertView;
+		DriverCarItem v = (DriverCarItem) convertView;
+		
 		if (v == null) {
-			LayoutInflater vi = (LayoutInflater) getContext().getSystemService(
-					Context.LAYOUT_INFLATER_SERVICE);
-			v = vi.inflate(R.layout.view_driver_car_item, null);
+			v = new DriverCarItem(getContext(), null);
 		}
+		
 		DriverCar item = items.get(position);
+		
 		if (item != null) {
-			TextView name = (TextView) v
-					.findViewById(R.id.driver_car_item_textview_name);
-			TextView seats = (TextView) v
-					.findViewById(R.id.driver_car_item_textview_seats);
-			TextView trunk = (TextView) v
-					.findViewById(R.id.driver_car_item_textview_trunk);
-			ColorCheckBox color = (ColorCheckBox) v
-					.findViewById(R.id.driver_car_item_colorcheckbox_color);
-			ImageView defaultCar = (ImageView) v
-					.findViewById(R.id.driver_car_item_imageview_default);
-
-			if (name != null) {
-				name.setText(item.getBrand().getName() + ' '
-						+ item.getModel().getName());
-			}
-			if (seats != null) {
-				seats.setText(String.valueOf(item.getSeats()));
-			}
-			if (trunk != null) {
-				trunk.setText(item.getTrunk().getName());
-			}
-			if (color != null) {
-				color.setColor(item.getColor().getHex());
-			}
-			if (defaultCar != null) {
-				defaultCar.setVisibility(item.isDefaultCar() ? View.VISIBLE
-						: View.GONE);
-			}
+			v.setCar(item);
 		}
+		
 		return v;
 	}
 
---
+++ src/fr/utbm/carpooling/view/widgets/DriverCarItem.java
@@ -0,0 +1,67 @@
+package fr.utbm.carpooling.view.widgets;
+
+import fr.utbm.carpooling.R;
+import fr.utbm.carpooling.model.DriverCar;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class DriverCarItem extends TableLayout {
+	
+	protected DriverCar mCar = null;
+	
+	protected TextView mName = null;
+	protected ColorCheckBox mColor = null;
+	protected TextView mSeats = null;
+	protected TextView mTrunkSize = null;
+	protected ImageView mDefaultCar = null;
+	
+	public DriverCarItem(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_driver_car_item, this, true);
+        
+		initItems();
+	}
+	
+	public void setCar(DriverCar car) {
+		mCar = car;
+		initView();
+	}
+	
+	protected void initItems() {
+		mName = (TextView) ((ViewGroup) getChildAt(0)).getChildAt(0);
+        mColor = (ColorCheckBox) ((ViewGroup) getChildAt(1)).getChildAt(0);
+        mSeats = (TextView) ((ViewGroup) getChildAt(1)).getChildAt(2);
+        mTrunkSize = (TextView) ((ViewGroup) getChildAt(1)).getChildAt(4);
+        mDefaultCar = (ImageView) ((ViewGroup) getChildAt(1)).getChildAt(5);
+	}
+	
+	protected void initView() {
+		if (mName != null) {
+			mName.setText(mCar.getBrand().getName() + ' ' + mCar.getModel().getName());
+		}
+	
+		if (mSeats != null) {
+			mSeats.setText(String.valueOf(mCar.getSeats()));
+		}
+		
+		if (mTrunkSize != null) {
+			mTrunkSize.setText(mCar.getTrunk().getName());
+		}
+		
+		if (mColor != null) {
+			mColor.setColor(mCar.getColor().getHex());
+		}
+		
+		if (mDefaultCar != null) {
+			mDefaultCar.setVisibility(mCar.isDefaultCar() ? View.VISIBLE : View.GONE);
+		}
+	}
+}
