a445e86f938686bbcba8205ff4267a9a196d22c6
Boko <bartheleway@live.fr>
2013-05-26 02:00:16 +0200

    Fix small bugs

--- src/fr/utbm/carpooling/view/widgets/TripPathRestrictedView.java
+++ src/fr/utbm/carpooling/view/widgets/TripPathRestrictedView.java
@@ -1,6 +1,5 @@
 package fr.utbm.carpooling.view.widgets;
 
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
@@ -14,46 +13,38 @@ public class TripPathRestrictedView extends TripPathView {
 
     private Checkpoint mLimit1 = null;
     private Checkpoint mLimit2 = null;
-
+    
     private boolean mLimit1IsBound;
     private boolean mLimit2IsBound;
-
-
+    
     public TripPathRestrictedView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
         drawPath(canvas);
 
         if (mSurroundBounds) {
-
             if (mLimit1IsBound)
                 drawBound1(canvas);
 
             if (mLimit2IsBound)
                 drawBound2(canvas);
         }
-
     }
 
     @Override
     public void setCheckpoints(List<Checkpoint> checkpoints) {
-
         LinkedList<Checkpoint> list = (LinkedList<Checkpoint>) checkpoints;
 
         if (mLimit1 != null && mLimit2 != null) {
-
             mLimit1IsBound = (list.getFirst() == mLimit1);
             mLimit2IsBound = (list.getLast() == mLimit2);
 
             try {
-
                 while (list.getFirst() != mLimit1)
                     list.removeFirst();
-
 
                 while (list.getLast() != mLimit2)
                     list.removeLast();
@@ -65,9 +56,7 @@ public class TripPathRestrictedView extends TripPathView {
     }
 
     public void setLimits(Checkpoint limit1, Checkpoint limit2) {
-
         mLimit1 = limit1;
         mLimit2 = limit2;
     }
-
 }
