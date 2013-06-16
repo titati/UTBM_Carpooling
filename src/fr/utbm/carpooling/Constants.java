8de2e0602804fa53e39c9244935b309fe639f044
Boko <bartheleway@live.fr>
2013-06-16 23:24:29 +0200

    Added JavaDoc + Complete constants

--- src/fr/utbm/carpooling/Constants.java
+++ src/fr/utbm/carpooling/Constants.java
@@ -1,7 +1,16 @@
 package fr.utbm.carpooling;
 
 public class Constants {
+	/** Define the name of the file where we store the user infos */
 	public static final String FILE_USER_INFO = "userInfos";
+	/** Define the name of the file where we store the color info */
 	public static final String FILE_COLORS = "colors";
+	/** Define the name of the file where we store the list of user's cars */
 	public static final String FILE_USER_CARS = "userCars";
+	/** Define the name of the file where we store the list of sites */
+	public static final String FILE_SITES = "userCars";
+	/** Define the name of the file where we store the list of brand */
+	public static final String FILE_BRANDS = "userCars";
+	/** Define the name of the file where we store the list of model for each brand */
+	public static final String FILE_MODELS = "userCars";
 }
